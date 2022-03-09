934697d7e97827508f46998844f20a9f8caad4a5
Adam Florez <asflorez@student.fullsail.edu>
2022-03-08 20:35:34 -0500

    update footer links

--- components/Footer.js
+++ components/Footer.js
@@ -6,13 +6,21 @@ const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className="flex justify-center items-center">
-        <a href="https://github.com/FlorezAdam-FS">
+        <a href="https://github.com/FlorezAdam-FS" target="_blank" noreferrer>
           <FiGithub className="text-white text-2xl mr-10 hover:text-orange" />
         </a>
-        <a href="https://www.instagram.com/adamflorezdesigns/">
+        <a
+          href="https://www.instagram.com/adamflorezdesigns/"
+          target="_blank"
+          noreferrer
+        >
           <FiInstagram className="text-white text-2xl mr-10 hover:text-orange" />
         </a>
-        <a href="https://www.linkedin.com/in/adamflorez/">
+        <a
+          href="https://www.linkedin.com/in/adamflorez/"
+          target="_blank"
+          noreferrer
+        >
           <FiLinkedin className="text-white text-2xl hover:text-orange" />
         </a>
       </div>
